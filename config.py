4c9ba81e13891ed922639b6731e255c00f715582
Devsoppros <102241186+Devsoppros@users.noreply.github.com>
2023-01-30 19:09:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
 import os
 
 class API:
-    API_ID = int(os.environ["8460373"])
-    API_HASH = os.environ["83d8e423197251216303abfcbed9e820"]
+    API_ID = int("8460373")
+    API_HASH = "83d8e423197251216303abfcbed9e820"
 
 class TOKENS:
-    BOT_TOKEN = os.environ["5913668020:AAHBpAu9HAtteTU20m4LSG3PZYfhRctsjBY"]
-    BOT_TOKEN_2 = os.environ["BOT_TOKEN_2"]
-    BOT_TOKEN_3 = os.environ["BOT_TOKEN_3"]
-    BOT_TOKEN_4 = os.environ["BOT_TOKEN_4"]
-    BOT_TOKEN_5 = os.environ["BOT_TOKEN_5"]
-    BOT_TOKEN_6 = os.environ["BOT_TOKEN_6"]
-    BOT_TOKEN_7 = os.environ["BOT_TOKEN_7"]
-    BOT_TOKEN_8 = os.environ["BOT_TOKEN_8"]
-    BOT_TOKEN_9 = os.environ["BOT_TOKEN_9"]
-    BOT_TOKEN_10 = os.environ["BOT_TOKEN_10"]
+    BOT_TOKEN = "5913668020:AAHBpAu9HAtteTU20m4LSG3PZYfhRctsjBY"
+    BOT_TOKEN_2 = "BOT_TOKEN_2"
+    BOT_TOKEN_3 = "BOT_TOKEN_3"
+    BOT_TOKEN_4 = "BOT_TOKEN_4"
+    BOT_TOKEN_5 = "BOT_TOKEN_5"
+    BOT_TOKEN_6 = "BOT_TOKEN_6"
+    BOT_TOKEN_7 = "BOT_TOKEN_7"
+    BOT_TOKEN_8 = "BOT_TOKEN_8"
+    BOT_TOKEN_9 = "BOT_TOKEN_9"
+    BOT_TOKEN_10 = "BOT_TOKEN_10"
 
 class DATABASE:
-    MONGO_DB_URL = os.environ["mongodb+srv://Lightyagami10:light@cluster0.zo6bf4g.mongodb.net/?retryWrites=true&w=majority"]
+    MONGO_DB_URL = "mongodb+srv://Lightyagami10:light@cluster0.zo6bf4g.mongodb.net/?retryWrites=true&w=majority"
 
 class DEV:
-    OWNER_ID = int(os.environ["1233641093"])
+    OWNER_ID = int("1233641093")
     
     # DONT EDIT THIS 
     SUDO_USERS = [5868832590] 
     # YOU CAN ADD SUDO USING /addsudo
 
 class STUFF:
-    ALIVE_PIC = os.environ["ALIVE_PIC"]
-    HELP_PIC = os.environ["HELP_PIC"]
-    START_PIC = os.environ["START_PIC"]
-    COMMAND_HANDLER = os.environ["/"]
+    ALIVE_PIC = "ALIVE_PIC"
+    HELP_PIC = "HELP_PIC"
+    START_PIC = "START_PIC"
+    COMMAND_HANDLER = "/"
